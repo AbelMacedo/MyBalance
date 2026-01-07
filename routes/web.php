37122d1c2dca10829e573450bd63792deea6c47c
Abel Macedo <perezmacedoabelyahir@gmail.com>
2026-01-06 22:23:02 -0600

    Feat: Preparacion de la base de datos, vistas y controladores para el registro e inicio de sesión

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::post('/login/authenticate', [AuthController::class, 'authenticate'])->name('login.authenticate');
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
